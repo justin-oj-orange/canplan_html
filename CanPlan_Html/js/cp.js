16c14a0aa7f9e74503cb56d6b6937bd87e882714
Justin Orange (OJ) <justin@karbonhq.com>
2019-06-19 21:30:26 +1200

    Initial COmmit

---
+++ CanPlan_Html/js/cp.js
@@ -0,0 +1,23 @@
+﻿(function () {
+    'use strict';
+    window.addEventListener('load', function () {
+        var forms = document.getElementsByClassName('needs-validation');
+        Array.prototype.filter.call(forms, function (form) {
+            form.addEventListener('submit', function (event) {
+                if (form.checkValidity() === false) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+                form.classList.add('was-validated');
+            }, false);
+        });
+
+        var subFormTogglebtns = document.getElementsByClassName('btnToggleSubform');
+        Array.prototype.filter.call(subFormTogglebtns, function (toggleBtn) {
+            toggleBtn.addEventListener('change', function () {
+                $(this.getAttribute('data-target')).find('[required]')[this.checked ? 'removeAttr': 'attr']('disabled', '');
+            });
+        });
+
+    }, false);
+})();
